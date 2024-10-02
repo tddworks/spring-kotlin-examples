f8f46d3fa74f3357a2b7e58a7535a761df1a55b8
slam <slamhan1987@gmail.com>
2024-10-02 17:04:11 +0800

    use @Entity replace @Table

--- src/main/kotlin/com/tddworks/spring/kotlin/examples/coroutines/user/api/User.kt
+++ src/main/kotlin/com/tddworks/spring/kotlin/examples/coroutines/user/api/User.kt
@@ -1,12 +1,28 @@
 package com.tddworks.spring.kotlin.examples.coroutines.user.api
 
-import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Table
+import co.touchlab.kermit.Logger
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.springframework.data.domain.AbstractAggregateRoot
+import org.springframework.data.domain.AfterDomainEventPublication
 
-@Table
+@Entity
 data class User(
     @Id
     val id: Long = 0,
     var username: String,
     var password: String
-)
+) : AbstractAggregateRoot<User>() {
+
+    fun register(): User {
+        registerEvent(UserCreatedEvent(this))
+        return this
+    }
+
+    @AfterDomainEventPublication
+    fun callback() {
+        Logger.i { "User registered: $this" }
+    }
+}
+
+data class UserCreatedEvent(val user: User)
